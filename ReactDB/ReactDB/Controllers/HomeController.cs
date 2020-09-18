54f603c23b7cf5ecfdfbd924deed4449703413c1
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-09-18 12:04:26 -0500

    got react to dynamically return data called from C# server - always remember to return your JSX elements

--- ReactDB/ReactDB/Controllers/HomeController.cs
+++ ReactDB/ReactDB/Controllers/HomeController.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReactDB.Models;
 
 namespace ReactDB.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly IList<WeatherModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<WeatherModel>
+            {
+                new WeatherModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new WeatherModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new WeatherModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
         }
     }
 }
--- ReactDB/ReactDB/wwwroot/js/tutorial.jsx
+++ ReactDB/ReactDB/wwwroot/js/tutorial.jsx
@@ -1,9 +1,68 @@
 ﻿class CommentBox extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { data: [] };
+
+        this.loadCommentsFromServer.bind(this);
+    }
+
+    loadCommentsFromServer() {
+        const xhr = new XMLHttpRequest();
+        console.log(this.props.url);
+        xhr.open('get', this.props.url, true);
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        };
+        xhr.send();
+    }
+
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        
+    }
+
     render() {
         return (
-            <div className="commentBox">Hello, world! I am a CommentBox.</div>
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList pollInterval={2000 } data={this.state.data} />
+            </div>
         );
     }
 }
 
-ReactDOM.render(<CommentBox />, document.getElementById('content'));
+class CommentList extends React.Component {
+    render() {
+        const { data } = this.props;;
+        return (
+            <div className="commentList">
+                {data.map(comment => {
+                    console.log(comment);
+                    return (
+                        <div key={comment.id}>
+
+                            <h1> bla {comment.author}</h1>
+                            <p> bla {comment.text}</p>
+
+                        </div>
+                    );
+                }
+                    )}
+            </div>
+        );
+    }
+}
+
+class Comment extends React.Component {
+    render() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">{this.props.author}</h2>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox url="/comments" />, document.getElementById('content'));
